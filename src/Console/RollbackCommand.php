2263eca175a5a5f6690198d4808b9be0bb36aba8
Jurre de Jongh <jurre@solestium.nl>
2018-07-30 11:35:34 +0200

    Added options

--- src/Console/RollbackCommand.php
+++ src/Console/RollbackCommand.php
@@ -106,4 +106,26 @@ class RollbackCommand extends BaseCommand
 
         $connection->reconnect('mysql');
     }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use.'],
+
+            ['force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production.'],
+
+            ['path', null, InputOption::VALUE_OPTIONAL, 'The path to the migrations files to be executed.'],
+
+            ['realpath', null, InputOption::VALUE_NONE, 'Indicate any provided migration file paths are pre-resolved absolute paths.'],
+
+            ['pretend', null, InputOption::VALUE_NONE, 'Dump the SQL queries that would be run.'],
+
+            ['step', null, InputOption::VALUE_OPTIONAL, 'The number of migrations to be reverted.'],
+        ];
+    }
 }
